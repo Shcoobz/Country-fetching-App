7e37f991432f2201b7df4338e7739502fe0718a6
Shcoobz <a.boeckmann23@gmail.com>
2024-01-04 15:18:00 +0100

    minor updates

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/more-on-fetch-the-countries-react-Shcoobz/',
+  base: '/Country-fetching-App/',
   build: {
     outDir: 'build',
   },
